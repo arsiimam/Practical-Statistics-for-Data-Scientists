2fb9458902a64f332c94ea89308b8a160b4ee5a3
arsiimam <arsiimam28@gmail.com>
2024-11-21 23:42:53 +0700

    Initiate Project. Add dataset and mean.py

---
+++ Estimates of Locations/mean.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+#import data set from data.csv as pandas DataFrame
+df = pd.read_csv('data.csv', delimiter=';')
+
+
+#Mean formulas using .mean() function
+mean_score = df['Score'].mean()
+print(mean_score)
+
+#Mean manual formulas
+mean_manual = df['Score'].sum() / df['Score'].count()
+print(mean_manual)
+
+#Mean with spesific category. For example : math
+mean_score_math = df[df['Subject'] == 'Math']['Score'].mean()
+print(mean_score_math)
+
+#Mean With Spesific Category and Dynamics Filter
+filer_subject = 'English' #You can  change this value to anything inside the subject
+mean_score_filtered = df[df['Subject'] == filer_subject]['Score'].mean()
+print(mean_score_filtered)
+
