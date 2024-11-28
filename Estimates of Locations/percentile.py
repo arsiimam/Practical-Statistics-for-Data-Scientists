d4cabf6f47f5e6baa9b944cd81f4d9bef94200a5
arsiimam <arsiimam28@gmail.com>
2024-11-28 23:43:31 +0700

    Add filter and dynamic filter for spesific subject in percentile.py

--- Estimates of Locations/percentile.py
+++ Estimates of Locations/percentile.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
+#Create Dataframe and import data.csv
 df = pd.read_csv('data.csv', delimiter=';')
 
 percentiles = [25, 50, 75]
 
 #calculate the 25th percentiles, 50th (median) and 75
 results = df['Score'].quantile([p / 100 for p in percentiles])
-
 print(results)
+
+#Calculate percentiles for with filter spesific subject
+results_subject= df[df['Subject']=='Math']['Score'].quantile([p / 100 for p in percentiles])
+print('Percentiles of Subject Math is: ',results_subject)
+
+#Calculate percenttiles using dynamic subject filter
+subject_filter = 'English'
+results_subject_dynamic = df[df['Subject']== subject_filter]['Score'].quantile([p / 100 for p in percentiles])
+print('Percentiles  for subject ',subject_filter, 'is:', results_subject_dynamic)
