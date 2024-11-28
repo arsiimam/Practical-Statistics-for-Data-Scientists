c8958d8daf031ed50d184d06b92753f4154f8dad
arsiimam <arsiimam28@gmail.com>
2024-11-28 23:49:44 +0700

    Added filter for spesific subject in trimed_mean.py

--- Estimates of Locations/Trimed_mean.py
+++ Estimates of Locations/Trimed_mean.py
@@ -4,10 +4,11 @@ from scipy.stats import trim_mean #using scipy
 df = pd.read_csv('data.csv', delimiter=';')
 
 #Specify the triming ratio
-
 triming_ratio = 0.1 #For example 10%
-
 trimmed_mean = trim_mean(df['Score'], triming_ratio)
-
 print(trimmed_mean)
+
+#Trimmed mean for spesific subject
+trimed_mean_subject = trim_mean(df[df['Subject']=='Math']['Score'], triming_ratio)
+print('Trimed meanfor subject math is: ',trimed_mean_subject)
 
