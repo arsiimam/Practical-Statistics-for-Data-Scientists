499cfceb1af30af4fe1a1529b50ab08837df8fae
arsiimam <arsiimam28@gmail.com>
2024-12-10 23:50:22 +0700

    Update summary.py

--- Estimates of Locations/summary.py
+++ Estimates of Locations/summary.py
@@ -18,3 +18,8 @@ print('Mode of the  score  is: ', mode)
 percentiles = [25, 50, 75]
 results = df['Score'].quantile([p / 100 for p in percentiles])
 print(results)
+
+#5. Trimmed Mean
+triming_ratio = 0.1 #For example 10%
+trimmed_mean = trim_mean(df['Score'], triming_ratio)
+print(trimmed_mean)
