c7146a362e2a38250d6c79569e731bf16188e45b
arsiimam <arsiimam28@gmail.com>
2024-12-11 23:58:40 +0700

    Added Summary_variability.py

--- Estimates of Locations/summary.py
+++ Estimates of Locations/summary.py
@@ -27,3 +27,14 @@ print(trimmed_mean)
 #6.Weighted mean
 weighted_mean = (df['Score'] * df['Weight']).sum() / df['Weight'].sum()
 print('Weighted Mean: ', weighted_mean)
+import pandas as pd
+import os
+
+file_path = os.path.join('..', 'Estimates of Locations', 'data.csv')
+df = pd.read_csv(file_path, delimiter=';')
+
+#Calculate the Mean
+mean = df['Score'].mean()
+#Calculate deviation of each value
+df['deviation'] = df['Score'] - mean
+print(df['Score'], df['deviation'])
