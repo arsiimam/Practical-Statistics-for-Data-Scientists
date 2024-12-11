e39ff87ee7438b634bfbd07c31f0d8b580170fd0
arsiimam <arsiimam28@gmail.com>
2024-12-12 00:03:22 +0700

    Added Summary_variability.py

---
+++ Estimates of Variability/summary_variability.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+
+file_path = os.path.join('..', 'Estimates of Locations', 'data.csv')
+df = pd.read_csv(file_path, delimiter=';')
+
+#Calculate the Mean
+mean = df['Score'].mean()
+#Calculate deviation of each value
+df['deviation'] = df['Score'] - mean
+print(df['Score'], df['deviation'])
