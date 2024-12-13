eff82845fb2d9b0fef1137a7b6ca9617437f963b
arsiimam <arsiimam28@gmail.com>
2024-12-13 23:39:00 +0700

    Update summary_variability.py

--- Estimates of Variability/summary_variability.py
+++ Estimates of Variability/summary_variability.py
@@ -4,8 +4,18 @@ import os
 file_path = os.path.join('..', 'Estimates of Locations', 'data.csv')
 df = pd.read_csv(file_path, delimiter=';')
 
+
+#1. Deviation
 #Calculate the Mean
 mean = df['Score'].mean()
 #Calculate deviation of each value
 df['deviation'] = df['Score'] - mean
 print(df['Score'], df['deviation'])
+
+#2.Std_deviation
+std_deviation = df['Score'].std(ddof=0)
+print('Standard Deviation : ', std_deviation)
+
+#3. Range
+range = df['Score'].max() - df['Score'].min()
+print('Range : ',range)
